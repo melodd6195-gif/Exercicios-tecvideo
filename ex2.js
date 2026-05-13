2aded3aa3f87f9d0c39c9f6ec493fb618ae52e16
Dennis <melodd6195@gmail.com>
2026-05-13 18:52:05 -0300

    teste

---
+++ ex2.js
@@ -0,0 +1,10 @@
+let num1 = 4
+let num2 = 6
+
+let soma = num1 + num2;
+let subtração = num1 - num2;
+let divisão = num1 / num2;
+
+console.log ('soma:', soma);
+console.log ('subtração', subtração);
+console.log ('divisão', divisão);
