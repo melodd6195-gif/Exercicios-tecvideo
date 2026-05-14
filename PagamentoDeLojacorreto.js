dac7cdf7b88c39f5a3cc2527b08536a8a86ab512
Dennis <melodd6195@gmail.com>
2026-05-14 17:08:53 -0300

    .

---
+++ PagamentoDeLojacorreto.js
@@ -0,0 +1,9 @@
+let preco = 60
+let aVista = 10
+let TotalaVista = 0
+
+totalaVista = (preco - (aVista / 100) * preco);
+
+console.log(`O preço é: R$${preco}`);
+console.log(`O preço aVista é: R$${totalaVista}`);
+console.log(`em 3x fica: R$${preco/3}`);
