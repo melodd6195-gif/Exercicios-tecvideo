e5d5e0e55be4e5f3d5b773a420271c987a49f3ce
Dennis <melodd6195@gmail.com>
2026-05-15 19:15:46 -0300

    .

---
+++ solveMeFirst-Funcao.js
@@ -0,0 +1,15 @@
+function soma (a, b) {
+ return a + b;
+}
+
+function menos (a, b) {
+  return a - b
+}
+
+function divisao (a, b) {
+  return a / b 
+}
+
+console.log(` o preço é fica: R$${soma(17, 4)}`);
+console.log(` O preco Avista fica: R$${menos(17, 5)}`)
+console.log(`o preco em 4x fica:  R$${divisao(17, 4)}`)
